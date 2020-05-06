3d73bee4d07b8e72e831a4e671973bb5e2086a46
whytepeter <peteremmanuelwhyte@gmail.com>
2020-05-06 19:25:10 +0100

    just finished the registration logic

--- auth.js
+++ auth.js
@@ -1,17 +1,17 @@
 //get inputs
-let form = document.querySelector("#createAccount")
+let form = document.querySelector("#register-form")
 let businessName = document.querySelector("#business-name")
 let email = document.querySelector("#business-email")
 let password = document.querySelector("#password")
-let country = document.querySelector("#input-country")
+let state = document.querySelector("#input-state")
 
 // User constructor
 class User {
-  constructor(name, email, password, country) {
+  constructor(name, email, password, state) {
     ;(this.name = name),
       (this.email = email),
       (this.password = password),
-      (this.country = country)
+      (this.state = state)
   }
 }
 
@@ -20,10 +20,11 @@ form.addEventListener("submit", (e) => {
   e.preventDefault()
 
   let emailPattern = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+
   let businessNameValue = businessName.value.trim()
   let emailValue = email.value.trim()
   let passwordValue = password.value.trim()
-  let countryValue = country.value.trim()
+  let stateValue = state.value.trim()
 
   if (businessNameValue == "") {
     alert("businessName must not be empty")
@@ -38,19 +39,20 @@ form.addEventListener("submit", (e) => {
     alert("password must be more than 6 digit")
     return false
   }
+
   //create new user
   const newUser = new User(
     businessNameValue,
     emailValue,
     passwordValue,
-    countryValue
+    stateValue
   )
 
   //clear fields
   businessName.value = ""
   email.value = ""
   password.value = ""
-  country.value = country.options[0].value
+  state.value = state.options[0].value
 
   //console.log(newUser)
   console.log(newUser)
