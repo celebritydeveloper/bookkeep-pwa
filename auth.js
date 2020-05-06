6f5e95749e05c2a35ff67177425875ce83e55f8d
whytepeter <peteremmanuelwhyte@gmail.com>
2020-05-06 23:03:22 +0100

    Registration logic is done

--- auth.js
+++ auth.js
@@ -1,9 +1,12 @@
 //get inputs
 let form = document.querySelector("#register-form")
-let businessName = document.querySelector("#business-name")
-let email = document.querySelector("#business-email")
-let password = document.querySelector("#password")
-let state = document.querySelector("#input-state")
+let businessName = document.querySelector("#validationServer01")
+let email = document.querySelector("#validationServer02")
+let password = document.querySelector("#validationServer03")
+let verifyPassword = document.querySelector("#validationServer04")
+let state = document.querySelector("#validationServer05")
+
+let inputs = [businessName, email, password, verifyPassword, state]
 
 // User constructor
 class User {
@@ -15,45 +18,133 @@ class User {
   }
 }
 
+//validate inputs on input and on blur
+inputs.forEach((input) => {
+  input.addEventListener("blur", () => {
+    validate(input)
+  })
+  input.addEventListener("input", () => {
+    validate(input)
+  })
+})
+
+//clear fields
+function clearFields() {
+  businessName.value = ""
+  email.value = ""
+  password.value = ""
+  verifyPassword.value = ""
+  state.value = state.options[0].value
+}
+
+// function validate form
+function validate(input) {
+  //get confirm password container
+  let confirmPasswordContainer = document.querySelector("#confirm-password")
+  let emailPattern = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+
+  if (input.id === "validationServer01") {
+    if (input.value.trim() !== "") {
+      //remove invalid class if its already there
+      input.classList.remove("is-invalid")
+      //add the valid class
+      input.classList.add("is-valid")
+      document.querySelector("#valid-feedback-1").classList.remove("d-none")
+      return true
+    } else {
+      input.classList.add("is-invalid")
+      document.querySelector("#valid-feedback-1").classList.add("d-none")
+      input.focus()
+      return false
+    }
+  } else if (input.id === "validationServer02") {
+    if (emailPattern.test(input.value.trim())) {
+      //remove invalid class if its already there
+      input.classList.remove("is-invalid")
+      //add the valid class
+      input.classList.add("is-valid")
+      document.querySelector("#valid-feedback-2").classList.remove("d-none")
+      return true
+    } else {
+      input.classList.add("is-invalid")
+      document.querySelector("#valid-feedback-2").classList.add("d-none")
+      input.focus()
+      return false
+    }
+  } else if (input.id === "validationServer03") {
+    if (input.value.trim().length > 6 && input.value.trim() !== "") {
+      //remove invalid class if its already there
+      input.classList.remove("is-invalid")
+      //add the valid class
+      input.classList.add("is-valid")
+      document.querySelector("#valid-feedback-3").classList.add("d-none")
+
+      //show confirm passwo
+      confirmPasswordContainer.classList.remove("d-none")
+      document.querySelector("#valid-feedback-3").classList.remove("d-none")
+      return true
+    } else {
+      input.classList.add("is-invalid")
+
+      //hide confirm password
+      confirmPasswordContainer.classList.add("d-none")
+      input.focus()
+      return false
+    }
+  } else if (input.id === "validationServer04") {
+    if (
+      input.value.trim() === password.value.trim() &&
+      input.value.trim() !== ""
+    ) {
+      //remove invalid class if its already there
+      input.classList.remove("is-invalid")
+      //add the valid class
+      input.classList.add("is-valid")
+      document.querySelector("#valid-feedback-4").classList.add("d-none")
+      return true
+    } else {
+      input.classList.add("is-invalid")
+      document.querySelector("#valid-feedback-4").classList.remove("d-none")
+      input.focus()
+      return false
+    }
+  } else if (input.id === "validationServer05") {
+    if (
+      input.value !== input.options[1].value &&
+      input.value !== input.options[0].value
+    ) {
+      //remove invalid class if its already there
+      input.classList.remove("is-invalid")
+      //add the valid class
+      input.classList.add("is-valid")
+      document.querySelector("#valid-feedback-5").classList.add("d-none")
+      return true
+    } else {
+      input.classList.add("is-invalid")
+      document.querySelector("#valid-feedback-5").classList.remove("d-none")
+      input.focus()
+      return false
+    }
+  }
+}
+
 // Event create a user
 form.addEventListener("submit", (e) => {
   e.preventDefault()
 
-  let emailPattern = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+  if (validate(businessName))
+    if (validate(email))
+      if (validate(password))
+        if (validate(verifyPassword))
+          if (validate(state)) {
+            let newUser = new User(
+              businessName.value,
+              email.value,
+              password.value,
+              state.value
+            )
 
-  let businessNameValue = businessName.value.trim()
-  let emailValue = email.value.trim()
-  let passwordValue = password.value.trim()
-  let stateValue = state.value.trim()
-
-  if (businessNameValue == "") {
-    alert("businessName must not be empty")
-    return false
-  } else if (!emailPattern.test(emailValue)) {
-    alert("email not valid")
-    return false
-  } else if (passwordValue == "") {
-    alert("password must not be empty")
-    return false
-  } else if (passwordValue.length < 6) {
-    alert("password must be more than 6 digit")
-    return false
-  }
-
-  //create new user
-  const newUser = new User(
-    businessNameValue,
-    emailValue,
-    passwordValue,
-    stateValue
-  )
-
-  //clear fields
-  businessName.value = ""
-  email.value = ""
-  password.value = ""
-  state.value = state.options[0].value
-
-  //console.log(newUser)
-  console.log(newUser)
+            //clear fields
+            clearFields()
+          }
 })
--- js/app.js
+++ js/app.js
@@ -1,35 +1,30 @@
 function populateState() {
-    let stateAPI = "http://locationsng-api.herokuapp.com/api/v1/states";
-    let states = new XMLHttpRequest;
+  let stateAPI = "http://locationsng-api.herokuapp.com/api/v1/states"
+  let states = new XMLHttpRequest()
 
-    states.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let state = JSON.parse(this.responseText);
+  states.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let state = JSON.parse(this.responseText)
 
-            state.forEach(location => {
-                let option = document.createElement("OPTION");
-                option.textContent = location.name;
-                const select = document.querySelector('#register-form #validationServer04');
+      state.forEach((location) => {
+        let option = document.createElement("OPTION")
+        option.textContent = location.name
+        const select = document.querySelector(
+          "#register-form #validationServer05"
+        )
 
+        select.appendChild(option)
 
+        console.log(location.name)
+      })
+    }
+  }
 
-                select.appendChild(option);
-                
-                console.log(location.name);
-                
-            });
-        }
-    };
-
-states.open("GET", stateAPI, true);
-states.send();
-
+  states.open("GET", stateAPI, true)
+  states.send()
 }
 
-populateState();
+populateState()
 
-
-
-const select = document.querySelector('#register-form #validationServer04');
-console.log(select);
-
+const select = document.querySelector("#register-form #validationServer05")
+console.log(select)
